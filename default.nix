dd6167c4eb854025c7a968c77e6adf10daf78274
FaustXVI <xavier.detant@gmail.com>
2019-11-19 20:44:28 +0100

    Make tcrdd work on any os with nix

--- default.nix
+++ default.nix
@@ -1,12 +1,17 @@
-{ stdenv }:
+{ pkgs ? (import <nixpkgs> {}), stdenv ? pkgs.stdenv }:
+
+with pkgs;
 
 stdenv.mkDerivation rec {
   name = "tcrdd";
   version = "latest";
-  buildInputs = [ ];
+  buildInputs = [ bash git ];
 
   src = ./.;
+  phases = "installPhase fixupPhase";
   installPhase = ''
+    mkdir tmp
+    export SHUNIT_TMPDIR=./tmp
     mkdir -p $out/bin
     cp $src/tcrdd.sh $out/bin/tcrdd
   '';
